1015df778583fb0557ef767dc741b1b3147b4b4c
moos <mooster@42at.com>
2010-06-16 02:02:13 -0700

    fixed scoping issue using dean edward's iframe method. (using Worker doesn't work since call must be synchronous) node-soup must be loaded in <body> (not <head>) to allow insertion of iframe.   - on dom load won't help since node starts immediately upon load passed test-script-* tests. added QUnit integration..... override 'assert' methods to get useful test info and count assertions.

--- test/index.js
+++ test/index.js
@@ -1,56 +1,139 @@
 // node-soup test module
 // signature: function (exports, require, module, __filename, __dirname)
 
-//console.log(this, arguments);//require, global, process, module);
-
 
 var
 	simple = {
-		path : '../deps/node/test/simple/test-',
-		tests : ('assert buffer byte-length #c-ares chdir'
-				+ ' #child-process-*'
-				+ ' #crypto #delayed-require'
-				+ ' #eio-* #error-reporting #eval-cx'
-				+ ' event-emitter-add-listeners event-emitter-modify-in-emit event-emitter-remove-listeners'
-				+ ' exception-handler #exec'
-				+ ' file-read-noexist #file-write-stream'
-				+ ' #fs-chmod fs-error-messages #fs-fsync fs-read-buffer fs-read fs-readfile-empty fs-stat'
-				+ ' #http-1.0 #http-304 http-cat http-chunked http-client-race http-client-race-2'
-				+ ' http-client-upload'
-//				+ ' #http-eof-on-connect ###http-exceptions'
-				)
-				.split(' ')
-				.reverse().slice(0,1)		// TEMP: run last for now!!!
+		path  : '../deps/node/test/simple/',
+		tests : _getFileContent('test-node-simple.txt').split('\n')
 	},
-	
 	fixtures = {
-		path : '../deps/node/test/fixtures/',
+		path  : '../deps/node/test/fixtures/',
 		tests : 'a echo exit'.split(' ')
 	};
 
 
-
-runTests(simple);
-//runTests(fixtures);
-
-
-function runTests(which){
-	which.tests.forEach(function(test,index){
-		
-		if (test[0] == '#') return;	// skip #test!!!!
-		
-		test = which.path + test;
-		try {
-			require(test);
-		} 
-		catch(e) {
-			console.warn('test failed: ', test, e);
-		}
-	})
+// filter hook into module
+var Module = module.constructor;
+Module.prototype._compile_orig = Module.prototype._compile;
+Module.prototype._compile = function (content, filename) {
+	if (filename in filters) {
+		content = filters[filename](content);
+	}
+	return this._compile_orig(content, filename); 
 }
 
+var filters = {};
+function filter_silent(content){
+	return (  "// test unit mods!\n"
+			+ "require.main.unitTest.enter('silence');\n"
+			+ content 
+			+  "\n\n;// test unit unmods!\n"
+			+ "require.main.unitTest.leave('silence');\n"
+			);
+}
 
+// load assert here so we can wrap its methods for verbose mode
+var assert = require('assert');
+(function(){
+	
+	wrapMethods(assert);
 
-///debugger;
+	function wrapMethods(exports) {
+		// wrap all methods
+		for (var k in exports){
+			var f = exports[k];
+			if (typeof f != 'function') continue;
+	
+			// wrap everything but AssertionError
+			if (f.name && f.name == 'AssertionError' ) continue;
+			
+			exports[k] = (function(f,k){	// closure for f,k
+				return makeWrapper(f,k); 
+			})(f,k);
+		}
+	}
+	function makeWrapper(f,k){
+		return function(){
+			if (require.main.unitTest.isVerbose() ) { 
+				QUnit.ok(true, k + '...' + QUnit.jsDump.parse(arguments) );
+				//console.log('wrapper::', f, arguments, module.id);
+			} else
+				++ require.main.unitTest.counter;
+			
+			f.apply(f,arguments);
+		}
+	}
+})();
+
+require.main.unitTest = {};
+require.main.unitTest.verbose = [];
+require.main.unitTest.counters = [];
+require.main.unitTest.counter = 0;
+require.main.unitTest.isVerbose = function(){
+	return this.verbose.length ? this.verbose[this.verbose.length-1] : true;
+}
+require.main.unitTest.enter = function(mode){
+	if (mode == 'silence') {
+		this.silencePrint(true);
+		this.verbose.push(false);
+		this.counter = 0;
+	} 
+}	
+require.main.unitTest.leave = function(mode){
+	if (mode == 'silence') {
+		this.verbose.pop(); 
+		this.silencePrint(false);			
+		this.counters.push(this.counter);
+	}
+}
+
+// 'print' silencer.  print is a local variable of calling scope (actully in ../common.js)
+require.main.unitTest.silencePrint = function(silent){
+	if (silent) {
+		if (!this._print) this._print = print;
+		print = function(){}	// empty
+	} else {
+		print = this._print;
+	}
+}
+
+function runTests(which){
+	var hide = false;
+	which.tests.some(function(name,index){
+		if (name.indexOf('#.show') == 0) {hide = false; return} 
+		if (name.indexOf('#.exit') == 0) {return true} 	// skip the rest
+		if (name.indexOf('#.hide') == 0) {hide = true}
+		if (hide) return;
+		
+		if (/^#?\s+|^\s*$/.test(name)) return; 			// skip blank lines and # comments
+		if (name[0] == '#') {							// skip #test 
+			QUnit.test(name + ' -- SKIPPED', function(){});
+			return;
+		}
+		var file = which.path + name.replace(/.js$/i, ''); // discard .js ext for require()
+		
+		QUnit.asyncTest(name, function(){
+			try {
+				var tmp = require(file);
+				var c = require.main.unitTest.counters.pop();
+				if (c) ok(true, 'success '+ name + ' ('+ c + ' assertions)' );
+			} 
+			catch(e) {
+				ok(false, e);
+			}
+			setTimeout(start, 500);
+			delete tmp;
+		})
+	});
+}
+
+filters[simple.path + 'test-buffer.js'] = filter_silent;
+	
+
+//run tests
+
+QUnit.module("simple");
+runTests(simple);
 
 
