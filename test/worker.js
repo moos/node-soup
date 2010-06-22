8dd0ed5d34b9cc499102a110a5f559295a1e6e1d
moos <mooster@42at.com>
2010-06-21 18:35:17 -0700

    added unhandled exception handling.

--- test/worker.js
+++ test/worker.js
@@ -5,13 +5,17 @@ if (typeof console == 'undefined') {
 	console.warn =  
 	console.error =
 		function(){
-			postMessage({type:'log', msg: JSON.stringify(Array.prototype.slice.call(arguments)) });
+			postMessage({
+				type:'log', 
+				msg: (QUnit && QUnit.jsDump.parse(arguments) ) 
+					|| JSON.stringify(Array.prototype.slice.call(arguments)) 
+				});
 		}
 }
 if (typeof alert == 'undefined') {
 	alert = console.log;
 }
-
+QUnit = null;
 process = {};
 process.argv = [];
 process.argv[1] = 'testrunner.js';	// loaded by node
@@ -21,15 +25,41 @@ process.env.NODE_DEBUG = 0;		// this must be set before loading node
 importScripts('../lib/soup-base-2.js');	// this is sync!
 
 
-console.log('hi ' , typeof global, typeof require, ''+module , typeof QUnit);
+//console.log('hi ' , typeof global, typeof require, ''+module , typeof QUnit);
 
 onmessage = function(ev){
 	var data = ev.data;
-	if (data.start) {
-		
-		console.log('starting ' + data.file);
 
+	if (data.type == 'start') {
+		console.log('starting ' + data.file);
 		runTest(data.file);
+		
+	} else if (data.type == 'uncaughtException') {
+		console.log('have error', data);
+		var handled = false;
+		try {
+			handled = process.emit(data.type, new Error(data.error.message));
+			
+			if (handled === false) {
+				postMessage({type:'fatal', msg: data.error.message });
+			}
+		} catch(e) {
+
+			console.log('no handler or rethrown', e);
+
+			postMessage({type:'error' , msg : new Error(e) });
+			
+			postMessage({type:'done', count: null});
+			// close the worker
+			close();			
+		}
 	}
 }
 
+// crashes FF!!!!!!!
+_onerror = function(ev) {
+	
+	console.log('>>>>>>>>>>> error in worker', ev);
+	
+	return true;
+}
