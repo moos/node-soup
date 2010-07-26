f1e5d0aa7308cc5c528d47bd94c16516a319785a
moos <mooster@42at.com>
2010-07-25 17:07:40 -0700

    console testing...

--- index.js
+++ index.js
@@ -4,5 +4,26 @@
 
 //require('./deps/node/test/simple/test-repl');
 
-require('./test/tests/node/test/simple/test-byte-length');
+//require('./test/tests/node/test/simple/test-byte-length');
 //require('./test/tests/node/test/simple/test-pump-file2tcp');
+
+
+
+http = require("http");
+
+var PORT = 80;
+
+
+  http.cat("http://localhost:"+PORT+"/", "utf8", function (err, content) {
+	  
+	  debugger;
+	  
+    if (err) {
+      throw err;
+    } else {
+      console.log("got response");
+    }
+  });
+
+
+  
